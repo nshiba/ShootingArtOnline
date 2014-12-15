e5d999e6775c823ceae0d31d16d7a7b829e0dc40
godslew <maousnake00@live.jp>
2014-12-15 15:23:28 +0900

    Game config update & add game package classes

--- NetBeansProject/ShootingArtOnline/src/config/GameConfig.java
+++ NetBeansProject/ShootingArtOnline/src/config/GameConfig.java
@@ -10,7 +10,9 @@ package config;
  */
 public class GameConfig {
 	
-	/* Constant */
+	/*** Constant ***/
+	/**base constant**/
+	/*base machine constant*/
 	/**
 	 * my Bullet Max Count
 	 */
@@ -31,8 +33,91 @@ public class GameConfig {
 	 * Base Max Energy
 	 */
 	final  public static int EnergyMax = 300;
+	
+	/**bullet constant**/
+	/*sniper bullet constant*/
 	/**
-	 * 
+	 * sniper energy use
 	 */
+	final public static int SniperEnergy = 30;
+	/**
+	 * sniper hit damge
+	 */
+	final public static int SniperDamage = 500;
+	/**
+	 * sniper down count
+	 */
+	final public static int SniperCombNum = 100;
+	/**
+	 * sniper speed
+	 */
+	final public static int SniperSpeed = 20;
+	/**
+	 * sniper cool time
+	 */
+	final public static int SniperTime = 50;
+	/*beam gun constant*/
+	/**
+	 * beam gun energy use
+	 */
+	final public static int BeamGunEnergy = 2;
+	/**
+	 * beam gun hit damage
+	 */
+	final public static int BeamGunDamage = 20;
+	/**
+	 * beam gun down count
+	 */
+	final public static int BeamGunCombNum = 4;
+	/**
+	 * beam gun speed
+	 */
+	final public static int BeamGunSpeed = 15;
+	/**
+	 * beam gun cool time
+	 */
+	final public static int BeamGunTime = 2;
+	/*big beam constant*/
+	/**
+	 * big beam energy use
+	 */
+	final public static int BigBeamEnergy = 80;
+	/**
+	 * big beam hit damage
+	 */
+	final public static int BigBeamDamage = 1000;
+	/**
+	 * big beam down count
+	 */
+	final public static int BigBeamCombNum = 100;
+	/**
+	 * big beam speed
+	 */
+	final public static int BigBeamSpeed = 15;
+	/**
+	 * big beam cool time
+	 */
+	final public static int BigBeamTime = 120;
+	
+	/**boost constant**/
+	/*base boost constant*/
+	/**
+	 * boost energy use
+	 */
+	final public static int BoostEnergy = 30;
+	/**
+	 * base machine boost use count
+	 */
+	final public static int BoostMaxUse = 3;
+	/**
+	 * boost speed(normal speed+)
+	 */
+	final public static int BoostSpeed = 10;
+	/**
+	 * boost time count (frame 1sec = 60 frame)
+	 */
+	final public static int BoostTime = 50;
+	
+	/**class**/
 	
 }
--- NetBeansProject/ShootingArtOnline/src/shootingartonline/TitleController.java
+++ NetBeansProject/ShootingArtOnline/src/shootingartonline/TitleController.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package game;
+package shootingartonline;
 
 import java.net.URL;
 import java.util.ResourceBundle;
