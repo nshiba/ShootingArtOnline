e3a424213f0b6ce9e9baffc8442636f92c55733b
naoya <naoya.shiba310@gmail.com>
2014-12-19 18:03:03 +0900

    main

--- NetBeansProject/ShootingArtOnline/src/shootingartonline/ShootingArtOnline.java
+++ NetBeansProject/ShootingArtOnline/src/shootingartonline/ShootingArtOnline.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shootingartonline;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Naoya
- */
 public class ShootingArtOnline extends Application {
 
 	@Override
